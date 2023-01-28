7c112ba6ba83ecb8f987135357da1f82520d6787
Gjorgji Kjosev <gorgi.kosev@gmail.com>
2023-01-28 21:16:47 +0000

    wip: add two tests

--- docker/run-docker-test.sh
+++ docker/run-docker-test.sh
@@ -39,7 +39,7 @@ echo Checking readiness via adb shell ls -d /sdcard/Android
 wait_available /sdcard/Android
 
 mkdir -p /adbfs
-./adbfs /adbfs
+/usr/bin/adbfs /adbfs
 
 echo Ready to run adbfs tests
 
@@ -47,10 +47,71 @@ BASE_DIR=/adbfs/sdcard/test
 
 test_mkdir() {
 
-  mkdir "$BASE_DIR/x"
-  output=$(ls -lad "$BASE_DIR/x")
-  # TODO: verify output is correct
+  local_timestamp=$(date "+%s")
 
+  mkdir "$BASE_DIR/x"
+  output=$(ls -lad --time-style="+%s" "$BASE_DIR/x")
+
+  timestamp=$(echo $output | cut -d' ' -f 6)
+  path=$(echo $output | cut -d' ' -f 7)
+
+  timestamp_diff=$((local_timestamp - timestamp))
+  abs_diff=${timestamp_diff#-}
+
+  if [ "$abs_diff" -gt 120 ];
+  then
+    echo "FAIL test_mkdir: file timestamp difference exceeds 120s: $abs_diff"
+    exit 1
+  fi
+
+  if [ "$path" != "$BASE_DIR/x" ];
+  then
+    echo "FAIL test_mkdir: unexpected path"
+    exit 1
+  fi
+
+  echo "PASS test_mkdir"
+}
+
+
+test_catfile() {
+
+  local_timestamp=$(date "+%s")
+
+  desired_content="Hello world"
+
+  echo "$desired_content" > "$BASE_DIR/file.txt"
+
+  output=$(ls -lad --time-style="+%s" "$BASE_DIR/file.txt")
+
+  timestamp=$(echo $output | cut -d' ' -f 6)
+  path=$(echo $output | cut -d' ' -f 7)
+
+  timestamp_diff=$((local_timestamp - timestamp))
+  abs_diff=${timestamp_diff#-}
+
+  if [ "$abs_diff" -gt 120 ];
+  then
+    echo "FAIL test_catfile: file timestamp difference exceeds 120s: $abs_diff"
+    exit 1
+  fi
+
+  if [ "$path" != "$BASE_DIR/file.txt" ];
+  then
+    echo "FAIL test_catfile: unexpected path"
+    exit 1
+  fi
+
+  file_contents=$(cat $BASE_DIR/file.txt)
+
+  if [ "$file_contents" != "Hello world" ]
+  then
+    echo "FAIL test_catfile: unexpected content: $file_contents"
+    echo "Expected: $desired_content"
+    exit 1
+  fi
+
+  echo "PASS test_catfile"
 }
 
 
@@ -58,16 +119,13 @@ test_mkdir() {
 mkdir "$BASE_DIR"
 
 test_mkdir
+test_catfile
 
+# todo
 
+# copy file with cp -a (archive, preserve timestamps)
 
-
-# todo, test mkdir
-
-# create file with cat
-
-# copy file
-
+# read a directory
 
 # touch to update time
 
@@ -75,4 +133,8 @@ test_mkdir
 
 rm -rf "$BASE_DIR"
 
+
+
+# rsync, both directions (compared against rsync of the very same tree on a "pure local" FS).
+# ompared to adb push --sync as well maybe
 
